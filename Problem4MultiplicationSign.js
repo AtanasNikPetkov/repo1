1cd5fe53fb6ec10700879356d6988a1903fc22c9
AtanasNikPetkov <103628337+AtanasNikPetkov@users.noreply.github.com>
2022-04-17 23:53:24 +0300

    Create Problem4MultiplicationSign.js

---
+++ Problem4MultiplicationSign.js
@@ -0,0 +1,28 @@
+let input=[
+    '-1',
+    '-0.5',
+    '-5.1',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0)
+
+let a=Number(gets());
+let b=Number(gets());
+let c=Number(gets());
+
+if (a>0){
+    if ((b>0 && c>0)||(b<0 && c<0)){
+        print('+');
+    }else if ((b<0 &&c>0)||(b>0 && c<0)){
+        print('-');
+    }
+}else if ( a<0){
+    if ((b>0 && c>0)||(b<0 && c<0)){
+        print('-');
+    }else if ((b<0 &&c>0)||(b>0 && c<0)){
+        print('+');
+    }
+}else if(a==0 || b ==0 || c==0){
+    print('0')
+}
